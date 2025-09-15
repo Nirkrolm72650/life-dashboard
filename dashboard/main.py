ce8556f3953a6553a071f12cb6ee0a477fd68434
Brandon Guyon <dev.brandonguyon@gmail.com>
2025-09-15 14:33:55 +0200

    Ajout du menu principal du dashboard

---
+++ dashboard/main.py
@@ -0,0 +1,35 @@
+def main():
+    while True:
+        print("#####################################")
+        print("### BIENVENUE DANS LIFE-DASHBOARD ###")
+        print("#####################################")
+        print("")
+        print("[1] - 💵 Finances")
+        print("[2] - 🏃 Sport & Santé")
+        print("[3] - 📆 Habitudes")
+        print("[4] - 🔎 Veille / YouTube")
+        print("[5] - ✅ Vérifier les mises à jours")
+        print("[6] - ❌ Quitter ")
+        print("")
+        
+        choice = input("> Votre choix : ")
+
+        if choice == "1":
+            pass
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5":
+            pass
+        elif choice == "6":
+            print("👋 Au revoir !")
+            break
+        else:
+            print(" ❌ Choix invalide. Réessayez.")
+
+
+if __name__ == "__main__":
+    main()
